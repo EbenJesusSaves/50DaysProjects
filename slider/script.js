f4e9023ac2d48e6146ea514d35fc2191eaa4a437
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2024-01-03 11:22:09 +0000

    styles

---
+++ slider/script.js
@@ -0,0 +1,8 @@
+const sliderContainer = document.getElementsByClassName("sliderContainer");
+const leftSlider = document.getElementsByClassName("leftSlider");
+const rightSlider = document.getElementsByClassName("rightSlider");
+const actionBtn = document.getElementsByClassName("actionBtn");
+const downBtn = document.getElementsByClassName("downBtn");
+const upBtn = document.getElementsByClassName("upBtn");
+
+const rightLength = rightSlider;
