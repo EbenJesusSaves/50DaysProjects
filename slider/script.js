5af04d48d646af93b34b40953fb25f5ca5495ea2
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2024-01-03 23:00:03 +0000

    completed day one project

--- slider/script.js
+++ slider/script.js
@@ -1,8 +1,37 @@
-const sliderContainer = document.getElementsByClassName("sliderContainer");
-const leftSlider = document.getElementsByClassName("leftSlider");
-const rightSlider = document.getElementsByClassName("rightSlider");
+const sliderContainer = document.querySelector(".sliderContainer");
+const leftSlider = document.querySelector(".leftSlider");
+const rightSlider = document.querySelector(".rightSlider");
 const actionBtn = document.getElementsByClassName("actionBtn");
-const downBtn = document.getElementsByClassName("downBtn");
-const upBtn = document.getElementsByClassName("upBtn");
+const downBtn = document.querySelector(".downBtn");
+const upBtn = document.querySelector(".upBtn");
 
-const rightLength = rightSlider;
+const rightLength = rightSlider.querySelectorAll("div").length;
+const leftLength = leftSlider.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+
+rightSlider.style.top = `-${(rightLength - 1) * 100}vh`;
+
+upBtn.addEventListener("click", () => changeSlide("up"));
+downBtn.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (btn) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (btn === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > rightLength - 1) {
+      activeSlideIndex = 0;
+    }
+  } else if (btn === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = rightLength - 1;
+    }
+  }
+  leftSlider.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  rightSlider.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
